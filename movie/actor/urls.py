c27370663a34f658dd179a4304988310dd2cb3ec
mh6497 <hoangminhhungcvp@gmail.com>
2020-02-20 08:33:44 +0700

    search actor function

--- movie/actor/urls.py
+++ movie/actor/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddActor,ViewActor
+from .views import AddActor, ViewActor, SearchActor
 
 app_name = "actor"
 urlpatterns = [
     path('add/', AddActor.as_view(), name="add"),
     path('view/a<int:actor_id>', ViewActor.as_view(), name="view"),
+    path('search/<str:keyword>', SearchActor.as_view(), name="search"),
 ]
--- movie/actor/views.py
+++ movie/actor/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import MultipleObjectsReturned
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
@@ -24,4 +25,14 @@ class ViewActor(View):
     def get(self, request, actor_id):
         actor = Actor.objects.get(pk=actor_id)
         return render(request, 'actor/view_actor.html', {'a': actor})
+
+
+class SearchActor(View):
+    def get(self, request, keyword):
+        try:
+            list_actor = Actor.objects.filter(name__contains=keyword)
+            return render(request, 'actor/search.html', {'actors': list_actor})
+        except MultipleObjectsReturned as e:
+            print(e)
+
 
