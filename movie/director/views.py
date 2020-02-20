d76cb7a772f6a0ab2e36fb10b70ced96aa8fdc8a
VuVanDuc <vuduc19081997@gmail.com>
2020-02-20 10:10:49 +0700

    Chi tiet, sua, xoa dao dien

--- movie/director/views.py
+++ movie/director/views.py
@@ -12,16 +12,39 @@ def add(request):
         form = DirectorAddForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect("/director")
     return render(request, 'director/add.html', {"form": form})
 
 
-def edit(request):
-    director = Director.objects.get()
-    return render(request, 'director/edit.html')
+def edit(request, pk):
+    director = Director.objects.get(pk=pk)
+    if request.method == "POST":
+        form = DirectorAddForm(request.POST)
+        if form.is_valid():
+            director.name = request.POST.get("name")
+            director.dob = request.POST.get("dob")
+            director.nationality = request.POST.get("nationality")
+            director.save()
+            return HttpResponseRedirect("/director")
+    return render(request, 'director/edit.html', {"director": director})
 
 
 def list(request):
     directors = Director.objects.all()
     return render(request, 'director/view.html', {"directors": directors})
+
+
+def delete(request, pk):
+    director = Director.objects.get(pk=pk)
+    director.delete()
+    return HttpResponseRedirect("/director")
+
+
+def detail(request, pk):
+    director = Director.objects.get(pk=pk)
+    return render(request, 'director/detail.html', {"director": director})
+
+
+def search(request):
+    directors = Director.objects.filter(name__contains="")
 
