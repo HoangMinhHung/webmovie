bffab001b4efc30be457880e4f75c6598a529f2b
mh6497 <hoangminhhungcvp@gmail.com>
2020-02-21 11:40:49 +0700

    fix bug pycharm

--- movie/review/models.py
+++ movie/review/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from movie.user.models import MovieUser
-from movie.director.models import Director
-from movie.actor.models import Actor
-from movie.category.models import Category
-from movie.type.models import Type
+from director.models import Director
+from actor.models import Actor
+from category.models import Category
+from type.models import Type
+from user.models import MovieUser
 # Create your models here.
 
 
@@ -12,16 +12,16 @@ class Movie(models.Model):
     description = models.TextField(max_length=255)
     movie_url = models.CharField(max_length=255)
     image_url = models.CharField(max_length=255)
-    director = models.ForeignKey(Director, on_delete=models.SET_NULL(), null=True)
-    actor = models.ForeignKey(Actor, on_delete=models.SET_NULL(), null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL(), null=True)
-    type = models.ForeignKey(Type, on_delete=models.SET_NULL(), null=True)
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
+    actor = models.ForeignKey(Actor, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
 
 
 class Review(models.Model):
     star = models.IntegerField(max_length=1, null=False)
-    user = models.ForeignKey(MovieUser, on_delete=models.SET_NULL(), null=True)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE())
+    user = models.ForeignKey(MovieUser, on_delete=models.SET_NULL, null=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
 
 
