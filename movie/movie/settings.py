051dc7a7adbe3ef1da67f57eec16baf75767204b
VuVanDuc <vuduc19081997@gmail.com>
2020-02-21 15:39:03 +0700

    --no-commit

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'category',
     'type',
     'review',
-    'type',
 ]
 
 MIDDLEWARE = [
--- movie/type/urls.py
+++ movie/type/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
+urlpatterns = [
     path('', views.list, name='type'),
     path("edit/<int:pk>", views.edit, name='edit'),
     path('delete/<int:pk>', views.delete, name='delete'),
