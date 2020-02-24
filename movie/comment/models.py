a51d0e4bdbe1e50b63b4e6c9cfe2ace8e9f174e6
VuVanDuc <vuduc19081997@gmail.com>
2020-02-24 13:17:26 +0700

    --no-commit

--- movie/comment/models.py
+++ movie/comment/models.py
@@ -8,3 +8,10 @@ class Comment(models.Model):
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
     user = models.ForeignKey(MovieUser, on_delete=models.SET_NULL, null=True)
+    episode =
+
+    class Meta:
+        ordering = ["date"]
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.user.name}"
