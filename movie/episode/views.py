fea8e66429d8af5c239fc7f60902c01f44929763
kentranptit <kentran24797@gmail.com>
2020-02-24 14:26:39 +0700

    Add, update, delete episodes function

--- movie/episode/views.py
+++ movie/episode/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Episode
+from .forms import EpisodeAddForm
 
 # Create your views here.
+def add(request):
+    form = EpisodeAddForm()
+    if request.method == "POST":
+        form = EpisodeAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/episode")
+    return render(request, 'episode/add.html', {"form": form})
+
+
+def edit(request, pk):
+    episode = EpisodeAddForm.objects.get(pk=pk)
+    if request.method == "POST":
+        form = EpisodeAddForm(request.POST)
+        if form.is_valid():
+            episode.name = request.POST.get("name")
+            episode.episode_url = request.POST.get("episode_url")
+            director.save()
+            return HttpResponseRedirect("/episode")
+    return render(request, 'episode/edit.html', {"episode": episode})
+
+def list(request):
+    episodes = Episode.objects.all()
+    return render(request, 'episode/view.html', {"episodes": episodes})
+
+def delete(request, pk):
+    episode = Episode.objects.get(pk=pk)
+    episode.delete()
+    return HttpResponseRedirect("/episode")
+
+def detail(request, pk):
+    episode = Episode.objects.get(pk=pk)
+    return render(request, 'episode/detail.html', {"episode": episode})
+
+def search(request):
+    if request.method == "POST":
+        form = request.POST
+        # if form.is_valid:
+        keyword = form["search"]
+        episodes = Episode.objects.filter(name__contains=keyword)
+        return render(request, 'episode/view.html', {"episodes": episodes})
