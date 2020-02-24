a142488f4faaa18a064fe5023547f39f8c977241
mh6497 <hoangminhhungcvp@gmail.com>
2020-02-24 11:37:21 +0700

    --no-commit

--- movie/director/models.py
+++ movie/director/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Director(models.Model):
     name = models.CharField(max_length=50)
     dob = models.DateField()
-    nationality = models.CharField(max_length=50)
+    nationality = models.CharField(max_length = 50)
 
     def __str__(self):
         return self.name
