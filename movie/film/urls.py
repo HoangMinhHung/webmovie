317368ce7dae97fa3c2878b2021349539b653973
mh6497 <hoangminhhungcvp@gmail.com>
2020-02-24 08:22:03 +0700

    rewrite url rating

--- movie/film/urls.py
+++ movie/film/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import RatingMovie
-from .models import Review
 
 urlpatterns = [
     path('rating/', RatingMovie.as_view(), name='rating'),
--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'director',
     'category',
     'type',
-    'review',
+    'film',
 ]
 
 MIDDLEWARE = [
