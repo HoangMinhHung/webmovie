36496d29fba0c3349ae7a550a51873ef7eafd66c
mh6497 <hoangminhhungcvp@gmail.com>
2020-02-24 10:48:10 +0700

    change model movie

--- movie/film/forms.py
+++ movie/film/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
-        fields = ("title", "description", "movie_url", "image_url", "director", "actor", "category", "type")
+        fields = ("title", "description", "movie_url", "image_url", "director", "category", "type")
