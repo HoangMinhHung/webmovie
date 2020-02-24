6c44a942c413f9fa055d7c11a9305925e5257c03
VuVanDuc <vuduc19081997@gmail.com>
2020-02-24 14:08:33 +0700

    get master of hunghm

--- movie/comment/views.py
+++ movie/comment/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
-def add(request):
-    render(request, 'comment/add.html')
+from episode.models import Episode
+
+from comment.forms import CommentForm
+
+
+def episode_detail(request, slug):
+    template_name = 'post_detail.html'
+    episode = get_object_or_404(Episode, slug=slug)
+    comments = episode.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.episode = episode
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'episode': episode,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
