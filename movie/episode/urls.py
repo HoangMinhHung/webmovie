2fa573c630b73d0d62fb5f6fc145e6509cb69775
kentranptit <kentran24797@gmail.com>
2020-02-24 13:01:06 +0700

    Create model Episode

---
+++ movie/episode/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('add/', views.add, name='add'),
+    path('', views.list, name='view'),
+    path('edit/<int:pk>', views.edit, name='edit'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+    path('<int:pk>', views.detail, name='detail'),
+    path('search/', views.search, name='search'),
+]
