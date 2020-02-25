0c392aed8bb90e48226d201adcdde38813210fe3
VuVanDuc <vuduc19081997@gmail.com>
2020-02-25 11:04:09 +0700

    merge film + comment

--- movie/film/admin.py
+++ movie/film/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Movie, Review
+from .models import Movie, Review, Comment
+
 # Register your models here.
 
 admin.site.register(Movie)
 admin.site.register(Review)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["user", "content", "date", "active"]
+    list_filter = ["active", "date"]
+    search_fields = ['user', 'date', 'content']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
+admin.site.register(Comment, CommentAdmin)
