788b8b387d42d1e3b08a0545a9358fae7759e15c
VuVanDuc <vuduc19081997@gmail.com>
2020-02-25 11:32:58 +0700

    merge episode + comment

--- movie/comment/admin.py
+++ movie/comment/admin.py
@@ -1,17 +1,17 @@
-from django.contrib import admin
-
-# Register your models here.
-from comment.models import Comment
-
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ["user", "content", "date", "active"]
-    list_filter = ["active", "date"]
-    search_fields = ['user', 'date', 'content']
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(active=True)
-
-
-admin.site.register(Comment, CommentAdmin)
+# from django.contrib import admin
+#
+# # Register your models here.
+# from comment.models import Comment
+#
+#
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ["user", "content", "date", "active"]
+#     list_filter = ["active", "date"]
+#     search_fields = ['user', 'date', 'content']
+#     actions = ['approve_comments']
+#
+#     def approve_comments(self, request, queryset):
+#         queryset.update(active=True)
+#
+#
+# admin.site.register(Comment, CommentAdmin)
