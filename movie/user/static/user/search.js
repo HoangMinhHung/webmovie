9e62f6400ac0173bb3b01cce381d03c9439902d9
mh6497 <hoangminhhungcvp@gmail.com>
2020-02-27 09:39:28 +0700

    search keyup

--- movie/user/static/user/search.js
+++ movie/user/static/user/search.js
@@ -2,3 +2,8 @@ $('#btn-search').click(function () {
    var input = $('#input-search').val();
     $('#search-form').submit();
 });
+
+$('#input-search').keyup(function () {
+   var input = $('#input-search').val();
+    $('#search-form').submit();
+});
