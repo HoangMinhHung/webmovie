382001d03ae93b06d159e1915bb7273e335d5438
kentranptit <kentran24797@gmail.com>
2020-02-27 13:10:28 +0700

    --no-commit

--- movie/user/static/user/search.js
+++ movie/user/static/user/search.js
@@ -1,17 +1,6 @@
 $('#btn-search').click(function () {
-<<<<<<< HEAD
-   var input = $('#input-search').val();
-    $('#search-form').submit();
-});
-
-$('#input-search').keyup(function () {
-   var input = $('#input-search').val();
-    $('#search-form').submit();
-});
-=======
    var input = $('#txtSearch').val();
     $('#search-form').submit();
 });
 
 
->>>>>>> b6278122995ff4ddccd574bf7b77ef155aabf88d
