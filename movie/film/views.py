63e74d1b0d293b374dd3a75b4b3181afe2ea725f
VuVanDuc <vuduc19081997@gmail.com>
2020-02-27 11:34:40 +0700

    them danh sach tap

--- movie/film/views.py
+++ movie/film/views.py
@@ -12,9 +12,7 @@ from category.models import Category
 from type.models import Type
 
 from .models import Movie
-
-
-
+from episode.models import Episode
 
 
 class AddMovie(View):
@@ -47,6 +45,7 @@ def searchMovie(request, keyword):
 class RatingMovie(View):
     def get(self, request, pk):
         movie = Movie.objects.get(pk=pk)
+        episodes = Episode.objects.filter(movie__id = pk).order_by("-name")[0:3]
         reviews = Review.objects.filter(movie__id = pk)
         ave = reviews.aggregate(Avg('star')).get("star__avg")
         count = reviews.count()
@@ -54,7 +53,7 @@ class RatingMovie(View):
             ave = 0
             count = 0
         # print(ave)
-        return render(request, 'film/movie_detail.html', {"movie": movie, "count": count, "rate": round(ave)})
+        return render(request, 'film/movie_detail.html', {"movie": movie, "count": count, "rate": round(ave), "episodes": episodes})
 
     def post(self, request, pk):
         value = int(request.POST['get_star'])
