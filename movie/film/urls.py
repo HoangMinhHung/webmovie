bb1d63c1fb006ec49fe77428b401e9e08105bf45
VuVanDuc <vuduc19081997@gmail.com>
2020-03-02 11:05:13 +0700

    pagination film

--- movie/film/urls.py
+++ movie/film/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import RatingMovie, AddMovie
+from .views import RatingMovie, AddMovie, FilmList
 from . import views as film_views
 from episode import views as episode_views
 app_name = "film"
 urlpatterns = [
-    path('', film_views.index, name='index'),
+    path('', FilmList.as_view(), name='index'),
     path('<int:pk>', RatingMovie.as_view(), name='detail'),
     path('<int:pk1>/episode/<int:pk2>', episode_views.watch, name='watch'),
 ]
