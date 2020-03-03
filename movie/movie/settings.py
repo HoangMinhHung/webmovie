60a7506d9da9e0594ccb3c0ca67a9c1ba978f861
mh6497 <hoangminhhungcvp@gmail.com>
2020-03-03 14:27:35 +0700

    confirm email when registry

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -141,9 +141,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'Nguyenbinhnguyen0647@gmail.com'
+EMAIL_HOST_PASSWORD = 'Binhnguyen9647'
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
